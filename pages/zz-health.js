4f433db7974e901e8aafbcc2fa96601a475f5853
ross178 <ross@glasgowslides.co.uk>
2025-09-05 00:22:38 +0100

    chore: add zz-health route

--- pages/zz-health.js
+++ pages/zz-health.js
@@ -1,1 +1,9 @@
+export default function Health() {
+  return (
+    <main style={{maxWidth:900,margin:"0 auto",padding:24}}>
+      <h1>OK</h1>
+      <p>This proves the build is using this pages folder.</p>
+    </main>
+  );
+}
 
